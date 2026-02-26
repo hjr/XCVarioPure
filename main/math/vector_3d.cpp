9e578fc8d706dfb526136c2ffda334b83d21b0a5
Helmut J. Rohs <helmut.j.rohs@web.de>
2026-02-26 11:25:15 +0100

    Enriched the vector 3d features.

--- main/math/vector_3d.cpp
+++ main/math/vector_3d.cpp
@@ -45,6 +45,50 @@ vector_3d<T> vector_3d<T>::operator-(const vector_3d<T>& v2) const
 }
 
 template <typename T>
+vector_3d<T>& vector_3d<T>::operator*=(const vector_3d<T>& v2) {
+    x = x * v2.x;
+    y = y * v2.y;
+    z = z * v2.z;
+    return *this;
+}
+
+template <typename T>
+vector_3d<T> vector_3d<T>::operator*(const vector_3d<T>& v2) const {
+    vector_3d<T> result(*this);
+    return result *= v2;
+}
+
+template <typename T>
+vector_3d<T>& vector_3d<T>::operator/=(const vector_3d<T>& v2) {
+    x = x / v2.x;
+    y = y / v2.y;
+    z = z / v2.z;
+    return *this;
+}
+
+template <typename T>
+vector_3d<T> vector_3d<T>::operator/(const vector_3d<T>& v2) const {
+    vector_3d<T> result(*this);
+    return result /= v2;
+}
+
+template <typename T>
+vector_3d<T>& vector_3d<T>::operator+=(const T s2)
+{
+    x = x + s2;
+    y = y + s2;
+    z = z + s2;
+    return *this;
+}
+
+template <typename T>
+template <typename S>
+vector_3d<std::common_type_t<T, S>> vector_3d<T>::operator+(S s) const {
+    using R = std::common_type_t<T, S>;
+    return {static_cast<R>(x) + s, static_cast<R>(y) + s, static_cast<R>(z) + s};
+}
+
+template <typename T>
 vector_3d<T>& vector_3d<T>::operator*=(const T s2)
 {
     x = x * s2;
@@ -53,11 +97,18 @@ vector_3d<T>& vector_3d<T>::operator*=(const T s2)
     return *this;
 }
 
+// template <typename T>
+// vector_3d<T> vector_3d<T>::operator*(const T s2) const
+// {
+//     vector_3d<T> result(*this);
+//     return result *= s2;
+// }
 template <typename T>
-vector_3d<T> vector_3d<T>::operator*(const T s2) const
-{
-    vector_3d<T> result(*this);
-    return result *= s2;
+template <typename S>
+vector_3d<std::common_type_t<T,S>>
+vector_3d<T>::operator*(S s) const {
+    using R = std::common_type_t<T,S>;
+    return {static_cast<R>(x) * s, static_cast<R>(y) * s, static_cast<R>(z) * s};
 }
 
 template <typename T>
@@ -68,6 +119,14 @@ vector_3d<T>& vector_3d<T>::operator/=(const T s2)
     y = y / s2;
     z = z / s2;
     return *this;
+}
+
+template <typename T>
+template <typename S>
+vector_3d<std::common_type_t<T,S>>
+vector_3d<T>::operator/(S s) const {
+    using R = std::common_type_t<T,S>;
+    return {static_cast<R>(x) / s, static_cast<R>(y) / s, static_cast<R>(z) / s};
 }
 
 template <typename T>
@@ -108,12 +167,27 @@ vector_3d<T> vector_3d<T>::get_normalized() const
     return ret;
 }
 
+// template <typename T>
+// vector_3d<std::common_type_t<T, float>> vector_3d<T>::clamp(float max_norm) const {
+//     float n = get_norm();
+//     if (n <= max_norm)
+//         return *this;
+//     return (*this) * (max_norm / n);
+// }
+
 template <typename T>
-vector_3d<T> vector_3d<T>::clamp(float max_norm) const {
-    float n = get_norm();
-    if (n <= max_norm)
-        return *this;
-    return (*this) * (max_norm / n);
+void vector_3d<T>::clamp_inplace(float max_norm)
+{
+    float n = get_norm();   // norm() sollte float/double liefern
+
+    if (n <= max_norm || n < 1e-12f)
+        return;
+
+    float scale = max_norm / n;
+
+    x = static_cast<T>(x * scale);
+    y = static_cast<T>(y * scale);
+    z = static_cast<T>(z * scale);
 }
 
 template <typename T>
@@ -129,3 +203,12 @@ template class vector_3d<float>; // explicit instantiation
 template class vector_3d<double>;
 template class vector_3d<int>;
 template class vector_3d<int16_t>;
+
+template vector_3d<float>
+vector_3d<float>::operator+<float>(float) const;
+
+template vector_3d<float>
+vector_3d<float>::operator*<float>(float) const;
+
+template vector_3d<float>
+vector_3d<float>::operator/<float>(float) const;
--- main/math/vector_3d.h
+++ main/math/vector_3d.h
@@ -2,59 +2,72 @@
 
 #include <cmath>
 
-
 template <typename T>
-class vector_3d
-{
-public:
-	vector_3d() = default;
-	vector_3d( const vector_3d &o ) { x=o.x; y=o.y; z=o.z; };
-	vector_3d(T px, T py, T pz);
+class vector_3d {
+   public:
+    vector_3d() = default;
+    vector_3d(const vector_3d& o) {
+        x = o.x;
+        y = o.y;
+        z = o.z;
+    };
+    vector_3d(T px, T py, T pz);
     static inline vector_3d make_vector(int16_t, int16_t, int16_t, float) = delete;
-    vector_3d(vector_3d&&) = default; // Allow the move optimization
-	vector_3d<T>& operator=(const vector_3d<T>&) = default;
+    vector_3d(vector_3d&&) = default;  // Allow the move optimization
+    vector_3d<T>& operator=(const vector_3d<T>&) = default;
 
-	// indexed access
-	T& operator[](int i) { return (&x)[i]; }
+    // indexed access
+    T& operator[](int i) { return (&x)[i]; }
 
-	// API
-	void set(const T p1, const T p2, const T p3) { x=p1, y=p2, z=p3; }
-	vector_3d<T>& operator+=(const vector_3d<T>& v2);
-	vector_3d<T> operator+(const vector_3d<T>& v2) const;
-	vector_3d<T>& operator-=(const vector_3d<T>& v2);
-	vector_3d<T> operator-(const vector_3d<T>& v2) const;
-	vector_3d<T>& operator*=(const T s2);
-	vector_3d<T> operator*(const T s2) const;
-	vector_3d<T>& operator/=(const T s2);
-	T dot(const vector_3d<T>& v2) const;
-	vector_3d<T> cross(const vector_3d<T> &v2 ) const;
-	T get_norm() const { return std::sqrt(x*x + y*y + z*z); }
-	T get_norm2() const { return x*x + y*y + z*z; }
-	T normalize();
-	vector_3d<T> get_normalized() const;
-	vector_3d<T> clamp(float max_norm) const;
-	vector_3d<T> clamp(T minl, T maxl);
-	// Interpretation as Euler angles
-	float Roll()  const { return x; }
-	void setRoll(float roll) { x=roll; }
-	float Pitch() const { return y; }
-	void setPitch(float pitch) { y=pitch; }
-	float Yaw()   const { return z; }
-	void setYaw(float yaw) { z=yaw; }
+    // API
+    void set(const T p1, const T p2, const T p3) { x = p1, y = p2, z = p3; }
+    // element wise operators
+    vector_3d<T>& operator+=(const vector_3d<T>& v2);
+    vector_3d<T> operator+(const vector_3d<T>& v2) const;
+    vector_3d<T>& operator-=(const vector_3d<T>& v2);
+    vector_3d<T> operator-(const vector_3d<T>& v2) const;
+    vector_3d<T>& operator*=(const vector_3d<T>& v2);
+    vector_3d<T> operator*(const vector_3d<T>& v2) const;
+    vector_3d<T>& operator/=(const vector_3d<T>& v2);
+    vector_3d<T> operator/(const vector_3d<T>& v2) const;
+    // scalar operators
+    vector_3d<T>& operator+=(const T s2);
+    template <typename S>
+    vector_3d<std::common_type_t<T, S>> operator+(S s) const;
+    vector_3d<T>& operator*=(const T s2);
+    //	vector_3d<T> operator*(const T s2) const;
+    template <typename S>
+    vector_3d<std::common_type_t<T, S>> operator*(S s) const;
+    vector_3d<T>& operator/=(const T s2);
+    template <typename S>
+    vector_3d<std::common_type_t<T, S>> operator/(S s) const;
+    T dot(const vector_3d<T>& v2) const;
+    vector_3d<T> cross(const vector_3d<T>& v2) const;
+    std::common_type_t<T, float> get_norm() const { return std::sqrt(x * x + y * y + z * z); }
+    std::common_type_t<T, float> get_norm2() const { return x * x + y * y + z * z; }
+    T normalize();
+    vector_3d<T> get_normalized() const;
+    // vector_3d<std::common_type_t<T, float>> clamp(float max_norm) const;
+    void clamp_inplace(float max_norm);
+    vector_3d<T> clamp(T minl, T maxl);
+    // Interpretation as Euler angles
+    float Roll() const { return x; }
+    void setRoll(float roll) { x = roll; }
+    float Pitch() const { return y; }
+    void setPitch(float pitch) { y = pitch; }
+    float Yaw() const { return z; }
+    void setYaw(float yaw) { z = yaw; }
+    T operator[](int i) const { return (&x)[i]; }
 
-// todo private:
-	T x;
-	T y;
-	T z;
+    // todo private:
+    T x = T{};
+    T y = T{};
+    T z = T{};
 };
 
-template<>
+template <>
 inline vector_3d<float> vector_3d<float>::make_vector(int16_t a, int16_t b, int16_t c, float scale) {
     return {a * scale, b * scale, c * scale};
 }
 
-template <typename T>
-vector_3d<T>& operator+(const vector_3d<T> v1, const vector_3d<T> v2);
-
 #include "math/vector_3d_fwd.h"
-
