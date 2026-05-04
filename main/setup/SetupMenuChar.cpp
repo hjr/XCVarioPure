814c8fd76000a34ede752715b5b03bda82db0227
Helmut J. Rohs <helmut.j.rohs@web.de>
2026-05-04 09:02:38 +0200

    Exit setup after entering the expert code.

--- main/setup/SetupMenuChar.cpp
+++ main/setup/SetupMenuChar.cpp
@@ -177,6 +177,13 @@ void SetupMenuChar::longPress(){
     if (_dirty && bits._restart) {
         scheduleReboot();
     }
+    if (bits._end_menu) {
+        exit(2);
+        return;
+    } else if (bits._end_setup) {
+        exit(-1);
+        return;
+    }
     exit();
 }
 
